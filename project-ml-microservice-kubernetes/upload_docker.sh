dcb4f04126b1fe493f03bc47e267945b91d107f7
Naval Kishor Upadhyay <navalkishorupadhyay@Navals-MacBook-Air.local>
2020-08-12 14:49:02 +0530

    Initial commit.

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=navalkishorupadhyay/development
+
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag 5f64df8365d3 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
